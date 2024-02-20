2f9765767c4da142bfcfc854f40a827df2a1a6af
Timmytimi <adobeaustralia@hotmail.com>
2024-02-20 14:11:25 +0800

    “fix1.1”

---
+++ assets/01.html-pl--pZ8_.js
@@ -0,0 +1,98 @@
+import{_ as t,o as l,c as i,a as e}from"./app-nukdPxor.js";const n="/Timmyblog/assets/作业1-FBMpIDGs.jpg",s="/Timmyblog/assets/作业2-z6ZvQR4C.jpg",d="/Timmyblog/assets/作业3-VU3f9flN.jpg",a="/Timmyblog/assets/作业4-D0PruyEa.jpg",r={},v=e('<h1 id="网页编程html" tabindex="-1"><a class="header-anchor" href="#网页编程html" aria-hidden="true">#</a> 网页编程HTML</h1><h2 id="作业" tabindex="-1"><a class="header-anchor" href="#作业" aria-hidden="true">#</a> 作业</h2><h3 id="作业1的要求" tabindex="-1"><a class="header-anchor" href="#作业1的要求" aria-hidden="true">#</a> 作业1的要求：</h3><p><img src="'+n+`" alt="作业1"></p><p><em><strong>作业1我的代码：</strong></em></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;!DOCTYPE html&gt;
+&lt;html lang=&quot;en&quot;&gt;
+&lt;head&gt;
+    &lt;meta charset=&quot;UTF-8&quot;&gt;
+    &lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1.0&quot;&gt;
+    &lt;title&gt;Document&lt;/title&gt;
+&lt;/head&gt;
+&lt;body&gt;
+    &lt;ul type=&quot;circle&quot;&gt;
+        &lt;li&gt; &lt;strong&gt; 习近平为何赴山西大同考察&lt;/strong&gt;&lt;/li&gt;
+        &lt;li&gt; &lt;strong&gt; 一方土炕见证他的人民情怀&lt;/strong&gt;&lt;/li&gt;
+        &lt;li&gt; 习近平多次向他们表达敬意|为健康中国建设作新贡献&lt;/strong&gt;&lt;/li&gt;
+        &lt;li&gt; 全国幼儿园复学人数超390万 7省份返园复学&lt;/li&gt;
+        &lt;li&gt; 5.12全国防灾减灾日&lt;/li&gt;
+    &lt;/ul&gt;
+&lt;/body&gt;
+&lt;/html&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><em><strong>难点：ul 后面要加 type=&quot;circle&quot; 否则就是实心的黑点</strong></em></p><h3 id="作业2的要求" tabindex="-1"><a class="header-anchor" href="#作业2的要求" aria-hidden="true">#</a> 作业2的要求：</h3><p><img src="`+s+`" alt="作业2"></p><p><em><strong>作业2我的代码：</strong></em></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;!DOCTYPE html&gt;
+&lt;html lang=&quot;en&quot;&gt;
+&lt;head&gt;
+    &lt;meta charset=&quot;UTF-8&quot;&gt;
+    &lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1.0&quot;&gt;
+    &lt;title&gt;Document&lt;/title&gt;
+&lt;/head&gt;
+&lt;body&gt;
+    &lt;ul&gt;
+        &lt;li&gt;中国&lt;/li&gt;
+            &lt;ul type=&quot;circle&quot;&gt;
+                &lt;li&gt;北京&lt;/li&gt;
+                    &lt;ul type=&quot;square&quot;&gt;
+                        &lt;li&gt;东城&lt;/li&gt;
+                            &lt;ul type=&quot;square&quot;&gt;
+                                &lt;li&gt;天安门&lt;/li&gt;
+                            &lt;/ul&gt;
+                        &lt;li&gt;西城&lt;/li&gt;
+                        &lt;li&gt;宣武&lt;/li&gt;
+                    &lt;/ul&gt;
+                &lt;li&gt;天津&lt;/li&gt;
+                &lt;li&gt;河北&lt;/li&gt;
+                &lt;li&gt;上海&lt;/li&gt;
+            &lt;/ul&gt;
+        &lt;li&gt;俄罗斯&lt;/li&gt;
+        &lt;li&gt;美国&lt;/li&gt;
+    &lt;/ul&gt;
+&lt;/body&gt;
+&lt;/html&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><em><strong>难点：语法和框架，逻辑要清晰明确</strong></em></p><h3 id="作业3的要求" tabindex="-1"><a class="header-anchor" href="#作业3的要求" aria-hidden="true">#</a> 作业3的要求：</h3><p><img src="`+d+`" alt="作业3"></p><p><em><strong>作业3我的代码：</strong></em></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;!DOCTYPE html&gt;
+&lt;html lang=&quot;en&quot;&gt;
+&lt;head&gt;
+    &lt;meta charset=&quot;UTF-8&quot;&gt;
+    &lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1.0&quot;&gt;
+    &lt;title&gt;Document&lt;/title&gt;
+&lt;/head&gt;
+&lt;body&gt;
+    &lt;h1&gt;绿叶学习网问卷调查&lt;/h1&gt;
+    &lt;h2&gt;你通过什么途径来到绿叶学习网&lt;/h2&gt;
+    &lt;ol&gt;
+        &lt;li&gt;&lt;a href=&quot;www.baidu.com&quot;&gt;百度搜索&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href=&quot;www.baidu.com&quot;&gt;百度搜索&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href=&quot;www.baidu.com&quot;&gt;百度搜索&lt;/a&gt;&lt;/li&gt;
+    &lt;/ol&gt;
+&lt;/body&gt;
+&lt;/html&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><em><strong>难点：链接里的字要放在a标签里面</strong></em></p><h3 id="作业4的要求" tabindex="-1"><a class="header-anchor" href="#作业4的要求" aria-hidden="true">#</a> 作业4的要求：</h3><p><img src="`+a+`" alt="作业4"></p><p><em><strong>作业4我的代码：</strong></em></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;!DOCTYPE html&gt;
+&lt;html lang=&quot;en&quot;&gt;
+&lt;head&gt;
+    &lt;meta charset=&quot;UTF-8&quot;&gt;
+    &lt;meta name=&quot;viewport&quot; content=&quot;width=device-width, initial-scale=1.0&quot;&gt;
+    &lt;title&gt;Document&lt;/title&gt;
+&lt;/head&gt;
+&lt;body&gt;
+    &lt;h1&gt;腾讯科技高级web前端开发岗位&lt;/h1&gt;
+    &lt;hr&gt;
+
+    &lt;h2&gt;职位描述&lt;/h2&gt;
+    &lt;p&gt;负责重点项目的前端技术方案和架构的研发和维护工作；&lt;/p&gt;
+
+    &lt;h2&gt;岗位要求&lt;/h2&gt;
+    &lt;p&gt;
+        5年以上前端开发经验， &lt;strong&gt;精通html5/css3/javascript等&lt;/strong&gt; web开发技术；
+        &lt;br&gt;&lt;br&gt;
+        熟悉bootstrap，vue，angularjs，reactjs等框架，熟练掌握一种以上；
+        &lt;br&gt;&lt;br&gt;
+        代码⻛格严谨，能⾼保真还原设计稿，能兼容各种浏览器；
+        &lt;br&gt;&lt;br&gt;
+        对web前端的性能优化以及web常见漏洞有一定的理解和相关实践；
+        &lt;br&gt;&lt;br&gt;
+        具备良好的分析解决问题能力，能独立承担任务，有开发进度把控能力；
+        &lt;br&gt;&lt;br&gt;
+        责任心强，思路路清晰，抗压能力好，具备良好的对外沟通和团队协作能力。
+    &lt;/p&gt;
+    &lt;h2&gt;工作地址&lt;/h2&gt;
+    &lt;p&gt;上海市-徐汇区-腾云大厦&lt;/p&gt;
+    &lt;img src=&quot;./images/map.jpg&quot; alt=&quot;&quot;&gt;
+
+&lt;/body&gt;
+&lt;/html&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><em><strong>难点：图片插入以及br换行</strong></em></p>`,22),u=[v];function g(c,m){return l(),i("div",null,u)}const b=t(r,[["render",g],["__file","01.html.vue"]]);export{b as default};
